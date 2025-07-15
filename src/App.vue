a18dd8bd93922008f9676c1c83425226559513ce
BlackCoder56 <blackcoda56@gmail.com>
2025-07-15 18:12:17 +0300

    header section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
+  <Navbar />
   <h1 class="text-5xl font-bold underline text-slate-800">
     Hello world!
   </h1>
