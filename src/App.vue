b7db7191b69b2ae9b3863f6d0c7ea675dee500e7
BlackCoder56 <blackcoda56@gmail.com>
2025-07-16 23:24:38 +0300

    rockets section

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 // import TheWelcome from './components/TheWelcome.vue'
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import Rockets from './components/Rockets.vue';
 </script>
 
 <template>
   <Navbar />
   <main class="max-w-4xl mx-auto">
         <Hero />
+        <Rockets />
   </main>
   <!-- <h1 class="text-5xl font-bold underline text-slate-800">
     Hello world!
---
+++ src/components/Rockets.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="rockets" class="p-6 my-12 scroll-mt-20">
+            <h2 class="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
+                Our Rockets
+            </h2>
+            <ul class="list-none mx-auto my-12 flex flex-col sm:flex-row items-center gap-8">
+                <li class="w-2/3 sm:w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-black py-6 px-2 rounded-3xl shadow-xl">
+                    <img src="../assets/img/rocketman.png" alt="Explorer" class="w-1/2 mb-6">                                       
+                    <h3 class="text-3xl text-center text-slate-900 dark:text-white">Explorer</h3>
+                       <p class="hidden sm:block text-3xl text-center text-slate-500 dark:text-slate-400">$</p>
+                       <p class="sm:hidden text-3xl text-center text-slate-500 dark:text-slate-400">Affordable Exploration</p>
+                </li>
+
+                <li class="w-2/3 sm:w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-black py-6 px-2 rounded-3xl shadow-xl">
+                    <img src="../assets/img/rocketride.png" alt="Explorer" class="w-1/2 mb-6">                                       
+                    <h3 class="text-3xl text-center text-slate-900 dark:text-white">Adventure</h3>
+                       <p class="hidden sm:block text-3xl text-center text-slate-500 dark:text-slate-400">$$</p>
+                       <p class="sm:hidden text-3xl text-center text-slate-500 dark:text-slate-400">Best selling Rocket</p>
+                </li>
+
+                <li class="w-2/3 sm:w-5/6 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-black py-6 px-2 rounded-3xl shadow-xl">
+                    <img src="../assets/img/rocketlaunch.png" alt="infinity " class="w-1/2 mb-6">                                       
+                    <h3 class="text-3xl text-center text-slate-900 dark:text-white">Infinity</h3>
+                       <p class="hidden sm:block text-3xl text-center text-slate-500 dark:text-slate-400">$$$</p>
+                       <p class="sm:hidden text-3xl text-center text-slate-500 dark:text-slate-400">Luxury Starship Exploration</p>
+                </li>
+            </ul>
+
+        </section>
+
+</template>
