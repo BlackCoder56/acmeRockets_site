637c4b6b2ceb87a1124bb1e4a93a1d132f4c6469
BlackCoder56 <blackcoda56@gmail.com>
2025-07-16 09:24:02 +0300

    hero section

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
 
 <template>
   <Navbar />
-  <h1 class="text-5xl font-bold underline text-slate-800">
+  <main class="max-w-4xl mx-auto">
+        <Hero />
+  </main>
+  <!-- <h1 class="text-5xl font-bold underline text-slate-800">
     Hello world!
-  </h1>
+  </h1> -->
 </template>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="hero" class="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 scroll-mt-40 widescreen:section-min-height w-full h-screen">
+        <article class="sm:w-1/2">
+            <h2 class="max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+                We Boldy Go <span class="text-indigo-700 dark:text-indigo-300">Where No Rockets</span> Has Gone Before...
+            </h2>
+                <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                    We're building rockets for the next century today. From the moon to Mars, Jupiter and benyond...
+                </p>
+                <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                    Think Acme Rockets.
+                </p>
+        </article>
+        <img class="w-1/2" src="../assets/img/rocketdab.png" alt="Rocket Dab">
+    </section>
+</template>
